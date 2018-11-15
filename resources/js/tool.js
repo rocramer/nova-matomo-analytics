2f6f00f4f398a3e519ea014edf5d3f3ddba99975
Robin Cramer <robin.cramer@trivago.com>
2018-11-15 17:41:09 +0100

    Update metric

--- resources/js/tool.js
+++ resources/js/tool.js
@@ -1,10 +1,7 @@
 import CustomTrendMetric from './components/CustomTrendMetric'
-import TrendMetric from './components/Base/TrendMetric'
-
 
 Nova.booting((Vue, router) => {
 
-    Vue.component('trend-metric', TrendMetric);
     Vue.component('custom-trend-metric', CustomTrendMetric);
 
     //
